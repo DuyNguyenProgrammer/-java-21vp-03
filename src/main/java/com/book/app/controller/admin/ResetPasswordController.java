de6a15aa83e904cab7945b0401f7667e3dd6d17d
DuyNguyenProgrammer <124415567+DuyNguyenProgrammer@users.noreply.github.com>
2024-03-16 13:44:12 +0700

    add logout and change UI home

--- src/main/java/com/book/app/controller/admin/ResetPasswordController.java
+++ src/main/java/com/book/app/controller/admin/ResetPasswordController.java
@@ -2,6 +2,7 @@ package com.book.app.Controller.admin;
 
 import com.book.app.Dao.impl.EmployeeDaoImpl;
 import com.book.app.Entity.EmployeeEntity;
+import com.book.app.Utils.PasswordUtils;
 import com.book.app.Utils.SearchUtils;
 import com.book.app.Utils.SortUtils;
 import javafx.event.ActionEvent;
@@ -11,21 +12,26 @@ import javafx.scene.control.*;
 
 import java.io.IOException;
 import java.net.URL;
+import java.security.NoSuchAlgorithmException;
+import java.util.Objects;
 import java.util.ResourceBundle;
-import java.util.function.UnaryOperator;
 
-public class NewUserController implements Initializable {
+public class ResetPasswordController implements Initializable {
     @FXML
-    private TextField username, email, phone, address;
+    private PasswordField newPassword, confirmPassword;
     @FXML
-    private PasswordField password;
-    private TableView<EmployeeEntity> tableView;
+    private Button submitBtn;
+    private String oldData;
+    private int userId;
     private String oldSearch;
     private String oldSort;
     private Dialog<String> dialog;
+    private TableView<EmployeeEntity> tableView;
+    private EmployeeDaoImpl dao = new EmployeeDaoImpl();
     public String getOldSearch() {
         return oldSearch;
     }
+
     public void setOldSearch(String oldSearch) {
         this.oldSearch = oldSearch;
     }
@@ -38,8 +44,27 @@ public class NewUserController implements Initializable {
         this.oldSort = oldSort;
     }
 
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getOldData() {
+        return oldData;
+    }
+
+    public void setOldData(String oldData) {
+        this.oldData = oldData;
+    }
+
     public Dialog<String> getDialog() {
         return dialog;
+    }
+    public void setDialog(Dialog<String> dialog) {
+        this.dialog = dialog;
     }
     public TableView<EmployeeEntity> getTableView() {
         return tableView;
@@ -48,65 +73,71 @@ public class NewUserController implements Initializable {
     public void setTableView(TableView<EmployeeEntity> tableView) {
         this.tableView = tableView;
     }
-
-    public void setDialog(Dialog<String> dialog) {
-        this.dialog = dialog;
-    }
-
-    @FXML
-    private Button submitButton, cancel;
-    private EmployeeDaoImpl dao = new EmployeeDaoImpl();
-    @FXML
-    public void submit(ActionEvent event) throws IOException {
-            EmployeeEntity newUser = new EmployeeEntity();
-            newUser.setUsername(username.getText().trim());
-            newUser.setEmail(email.getText().trim());
-            newUser.setPhone(phone.getText().trim());
-            newUser.setAddress(address.getText().trim());
-            newUser.setPassword(password.getText().trim());
-            boolean result = dao.addEmployee(newUser);
-            if (result) {
-                this.dialog.setResult("successfully");
-                this.dialog.close();
-                tableView.setItems(SortUtils.getSortList(oldSort, SearchUtils.getAllUserOldSearch(oldSearch)));
-            }
-    }
     private boolean validateFields() {
-        return username.getText() != null && !username.getText().trim().isEmpty() &&
-                email.getText() != null && !email.getText().trim().isEmpty() &&
-                phone.getText() != null && !phone.getText().trim().isEmpty() &&
-                address.getText() != null && !address.getText().trim().isEmpty() &&
-                password.getText() != null && !password.getText().trim().isEmpty();
+        return newPassword.getText() != null && !newPassword.getText().trim().isEmpty() &&
+                confirmPassword.getText() != null && !confirmPassword.getText().trim().isEmpty();
     }
-    private void checkFields() {
-        submitButton.setDisable(!validateFields());
+    private boolean checkOldData() throws NoSuchAlgorithmException {
+        if (!Objects.isNull(oldData)) {
+            String newPass = Objects.requireNonNullElse(newPassword.getText(), "");
+            String confirmPass = Objects.requireNonNullElse(confirmPassword.getText(), "");
+
+
+            boolean newPassSameAsOldData = PasswordUtils.checkPassword(newPass.trim(),oldData);
+            boolean confirmPassSameAsOldData = PasswordUtils.checkPassword(confirmPass.trim(),oldData);;
+            boolean checkMatchNewPass = newPass.trim().equals(confirmPass);
+
+
+            return !(newPassSameAsOldData && confirmPassSameAsOldData && checkMatchNewPass);
+        } else {
+            return true;
+        }
+    }
+    private boolean checkComparePassword() {
+        if (validateFields()) {
+            return newPassword.getText().trim().equals(confirmPassword.getText().trim());
+        }
+        return false;
+    }
+    private void checkFields() throws NoSuchAlgorithmException {
+        submitBtn.setDisable(!checkOldData() || !validateFields() || checkComparePassword());
     }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        UnaryOperator<TextFormatter.Change> phoneFilter = change -> {
-            String text = change.getText();
-            if (text.matches("[0-9]*")) {
-                return change;
+        newPassword.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("^[a-zA-Z0-9]*$")) {
+                newPassword.setText(oldValue);
             }
-            return null;
-        };
-        TextFormatter<String> phoneFormatter = new TextFormatter<>(phoneFilter);
-        phone.setTextFormatter(phoneFormatter);
-        UnaryOperator<TextFormatter.Change> usernameFilter = change -> {
-            String text = change.getControlNewText();
-            if (text.isEmpty() || text.matches("^[a-zA-Z0-9]*$")) {
-                return change;
+            try {
+                checkFields();
+            } catch (NoSuchAlgorithmException e) {
+                throw new RuntimeException(e);
             }
-            return null;
-        };
-        TextFormatter<String> usernameFormatter = new TextFormatter<>(usernameFilter);
-        username.setTextFormatter(usernameFormatter);
-        username.textProperty().addListener((observable, oldValue, newValue) -> checkFields());
-        address.textProperty().addListener((observable, oldValue, newValue) -> checkFields());
-        email.textProperty().addListener((observable, oldValue, newValue) -> checkFields());
-        phone.textProperty().addListener((observable, oldValue, newValue) -> checkFields());
-        password.textProperty().addListener((observable, oldValue, newValue) -> checkFields());
+        });
 
-        checkFields();
+        confirmPassword.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("^[a-zA-Z0-9]*$")) {
+                confirmPassword.setText(oldValue);
+            }
+            try {
+                checkFields();
+            } catch (NoSuchAlgorithmException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        try {
+            checkFields();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @FXML
+    public void submit(ActionEvent event) throws IOException, NoSuchAlgorithmException {
+        boolean result = dao.resetPassword(userId, PasswordUtils.hashPassword(newPassword.getText().trim()));
+        if (result) {
+            this.dialog.setResult("successfully");
+            this.dialog.close();
+            tableView.setItems(SortUtils.getSortList(oldSort, SearchUtils.getAllUserOldSearch(oldSearch)));
+        }
     }
 }
