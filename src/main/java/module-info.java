f2c589ef8b1cbc50bb968aa2d82046906221ab92
DuyNguyenProgrammer <124415567+DuyNguyenProgrammer@users.noreply.github.com>
2024-03-31 03:53:44 +0700

    update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,24 @@
 module com.book.app {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+    requires java.sql;
+    requires de.jensd.fx.glyphs.fontawesome;
+    requires cloudinary.core;
+    requires dotenv.java;
+
     opens com.book.app to javafx.fxml;
     exports com.book.app;
-    exports com.book.app.controller;
-    opens com.book.app.controller to javafx.fxml;
+    exports com.book.app.Controller;
+    opens com.book.app.Controller to javafx.fxml;
+    opens com.book.app.Entity to javafx.base;
+//  admin
+    exports com.book.app.Controller.admin;
+    opens com.book.app.Controller.admin to javafx.fxml;
+//  employee
+    exports com.book.app.Controller.employee.author;
+    opens com.book.app.Controller.employee.author to javafx.fxml;
+    exports com.book.app.Controller.employee.category;
+    opens com.book.app.Controller.employee.category to javafx.fxml;
+    exports com.book.app.Controller.employee.publisher;
+    opens com.book.app.Controller.employee.publisher to javafx.fxml;
 }
