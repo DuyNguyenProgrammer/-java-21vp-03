dd2977a82c8ef70dc68451dc067d9a988c03100e
DuyNguyenProgrammer <124415567+DuyNguyenProgrammer@users.noreply.github.com>
2024-03-31 03:41:18 +0700

    update

--- src/main/java/com/book/app/Utils/DateUtils.java
+++ src/main/java/com/book/app/Utils/DateUtils.java
@@ -1,10 +1,11 @@
 package com.book.app.Utils;
 
+
 import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-
-// Lấy thời gian hiện tại
+//lay thoi gian hien tai
 public class DateUtils {
     public static String convertLocalDateToMmsString() {
         Instant instant = Instant.now();
@@ -15,12 +16,19 @@ public class DateUtils {
         // Chuyển đổi thành chuỗi
         return String.valueOf(milliseconds);
     }
-  
     public static String convertLocalDateToStringPattern(LocalDate localDate, String pattern) {
         if (localDate == null || pattern == null) {
             return null;
         }
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
         return localDate.format(formatter);
+    }
+    public static String convertLocalDateTimeToStringPattern(LocalDateTime dateTime, String pattern) {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+
+        // Chuyển đổi LocalDateTime thành chuỗi theo mẫu đã cho
+        return dateTime.format(formatter);
+
     }
 }
