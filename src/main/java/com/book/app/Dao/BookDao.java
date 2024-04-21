cbda4949310c3cd9c6b1e70f8d9b2d690c9ec765
DuyNguyenProgrammer <124415567+DuyNguyenProgrammer@users.noreply.github.com>
2024-04-21 22:54:35 +0700

    update

--- src/main/java/com/book/app/Dao/BookDao.java
+++ src/main/java/com/book/app/Dao/BookDao.java
@@ -5,9 +5,9 @@ import com.book.app.Entity.BookEntity;
 import java.util.List;
 
 public interface BookDao {
-    List<BookEntity> getAllBooks(String keyword, String sort);
+    List<BookEntity> getAllBook(String keyword, String sort);
     boolean addBook(BookEntity book);
     boolean editBook(BookEntity book);
-    boolean deleteBook(String id);
-    boolean hideOrShowBook(String id, boolean isEnable);
+    boolean lockOrUnLockBook(String id, Boolean enable);
+    BookEntity getBookDetail(String bookId);
 }
