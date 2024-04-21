08ebfbd7af944b2e31f052a371a1a6b0ab0be758
DuyNguyenProgrammer <124415567+DuyNguyenProgrammer@users.noreply.github.com>
2024-04-21 22:59:39 +0700

    update

--- src/main/java/com/book/app/Entity/BookEntity.java
+++ src/main/java/com/book/app/Entity/BookEntity.java
@@ -1,18 +1,78 @@
 package com.book.app.Entity;
 
-import java.time.LocalDateTime;
+import java.util.List;
 
-public class BookEntity {
+public class BookEntity  extends BaseEntity {
     private String id;
-    private String title;
-    private String authorName; // Changed from authorId
-    private String publisherName; // Changed from publisherId
-    private String categoryName; // Changed from categoryId
-    private String description;
-    private Boolean isEnable;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
+    private String image_url;
+    private String image_public_id;
+    private double price;
+    private int quantity;
+    private PublisherEntity publisher;
+    private List<AuthorEntity> authors;
+    private List<CategoryEntity> categories;
 
-    // Constructor, getters, and setters
-    // You can generate them using your IDE or write them manually
+    public List<CategoryEntity> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<CategoryEntity> categories) {
+        this.categories = categories;
+    }
+
+    public List<AuthorEntity> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<AuthorEntity> authors) {
+        this.authors = authors;
+    }
+
+    public PublisherEntity getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(PublisherEntity publisher) {
+        this.publisher = publisher;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
+
+    public String getImage_public_id() {
+        return image_public_id;
+    }
+
+    public void setImage_public_id(String image_public_id) {
+        this.image_public_id = image_public_id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
