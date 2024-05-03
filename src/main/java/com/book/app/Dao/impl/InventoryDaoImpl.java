e2e252281f792717138d4ee16e86953f6167c62e
Vinh9122 <136182120+Vinh9122@users.noreply.github.com>
2024-05-03 12:35:54 +0700

    Change inventory InventoryDaoImpl.java

--- src/main/java/com/book/app/Dao/impl/InventoryDaoImpl.java
+++ src/main/java/com/book/app/Dao/impl/InventoryDaoImpl.java
@@ -2,7 +2,9 @@ package com.book.app.Dao.impl;
 
 import com.book.app.Config.DBConnection;
 import com.book.app.Dao.InventoryDao;
+import com.book.app.Entity.BookEntity;
 import com.book.app.Entity.InventoryEntity;
+import com.book.app.Entity.PublisherEntity;
 import com.book.app.Utils.AppUtils;
 
 import java.sql.Connection;
@@ -47,5 +49,45 @@ public class InventoryDaoImpl implements InventoryDao {
     @Override
     public boolean addInventory(InventoryEntity inventory) {
         return false;
+    }
+
+    @Override
+    public List<BookEntity> getBookToInventory(List<String> listBookSelected) {
+        String sql = "SELECT b.book_id, b.title, b.url_image, p.name AS publisher" +
+                " FROM book b" +
+                " JOIN publisher p ON b.publisherId = p.publisher_id";
+        if (listBookSelected.size() > 0) {git add .
+            sql += " WHERE b.book_id NOT IN (";
+            for (int i = 0; i < listBookSelected.size(); i++) {
+                sql += " ?,";
+            }
+            sql = sql.substring(0, sql.length() - 1); // Xóa dấu phẩy cuối cùng
+            sql += " )";
+        }
+        List<BookEntity> listBook = new ArrayList<>();
+        try {
+            db.initPrepar(sql);
+            if (listBookSelected.size() > 0) {
+                for (int i = 0; i < listBookSelected.size(); i++) {
+                    db.getPreparedStatement().setString(i + 1, listBookSelected.get(i));
+                }
+            }
+            resultSet = db.executeSelect();
+            while (resultSet.next()){
+                BookEntity book = new BookEntity();
+                PublisherEntity publisher = new PublisherEntity();
+                book.setId(resultSet.getString("book_id"));
+                book.setName(resultSet.getString("title"));
+                publisher.setName(resultSet.getString("publisher"));
+                book.setPublisher(publisher);
+                book.setImage_url(resultSet.getString("url_image"));
+                listBook.add(book);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            db.closeConnection();
+        }
+        return listBook;
     }
 }
